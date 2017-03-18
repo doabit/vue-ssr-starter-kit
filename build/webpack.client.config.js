ad9810ee3c6805d1ee7dd34fc698d8e80c0dd7b6
doabit <doinsist@gmail.com>
2017-03-18 23:06:15 +0800

    Update to use vue-ssr-webpack-plugin and html-webpack-plugin

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,48 +1,47 @@
-const base = require('./webpack.base.config')
 const webpack = require('webpack')
+const merge = require('webpack-merge')
+const base = require('./webpack.base.config')
 const vueConfig = require('./vue-loader.config')
+const HTMLPlugin = require('html-webpack-plugin')
+const SWPrecachePlugin = require('sw-precache-webpack-plugin')
 
-
-const config = Object.assign({}, base, {
-  plugins: (base.plugins || []).concat([
-    // strip comments in Vue code
+const config = merge(base, {
+  resolve: {
+    alias: {
+      'create-api': './create-api-client.js'
+    }
+  },
+  plugins: [
+    // strip dev-only code in Vue source
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.VUE_ENV': '"client"'
     }),
     // extract vendor chunks for better caching
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'client-vendor-bundle.[hash].js'
+      name: 'vendor'
+    }),
+    // generate output HTML
+    new HTMLPlugin({
+      template: 'src/index.template.html'
     })
-  ])
+  ]
 })
 
 if (process.env.NODE_ENV === 'production') {
-  // extract CSS into a single file so it's applied on initial render
-  const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
-  vueConfig.loaders = {
-    css: ExtractTextPlugin.extract({
-      loader: 'css-loader',
-      fallbackLoader: 'vue-style-loader'
-    }),
-    stylus: ExtractTextPlugin.extract({
-      loader: 'css-loader!stylus-loader',
-      fallbackLoader: 'vue-style-loader'
-    })
-  }
-
   config.plugins.push(
-    new ExtractTextPlugin('styles.[hash].css'),
-    // this is needed in webpack 2 for minifying CSS
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    }),
     // minify JS
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       }
+    }),
+    // auto generate service worker
+    new SWPrecachePlugin({
+      cacheId: 'vue-hn',
+      filename: 'service-worker.js',
+      dontCacheBustUrlsMatching: /./,
+      staticFileGlobsIgnorePatterns: [/index\.html$/, /\.map$/]
     })
   )
 }
--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -1,19 +1,26 @@
 const webpack = require('webpack')
+const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
+const VueSSRPlugin = require('vue-ssr-webpack-plugin')
 
-module.exports = Object.assign({}, base, {
+module.exports = merge(base, {
   target: 'node',
-  devtool: false,
   entry: './src/server-entry.js',
-  output: Object.assign({}, base.output, {
+  output: {
     filename: 'server-bundle.js',
     libraryTarget: 'commonjs2'
-  }),
-  externals: ['axios'],
+  },
+  resolve: {
+    alias: {
+      'create-api': './create-api-server.js'
+    }
+  },
+  externals: Object.keys(require('../package.json').dependencies),
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"server"'
-    })
+    }),
+    new VueSSRPlugin()
   ]
 })

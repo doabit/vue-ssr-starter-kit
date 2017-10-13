29ffff1110b624dc12541192751e09a2eb47d3e2
doabit <doinsist@gmail.com>
2017-10-13 14:50:22 +0800

    Fix #26

--- src/client-entry.js
+++ src/client-entry.js
@@ -7,15 +7,14 @@ if (window.__INITIAL_STATE__) {
 }
 
 router.onReady(() => {
-  // 添加路由钩子函数，用于处理 asyncData.
-  // 在初始路由 resolve 后执行，
-  // 以便我们不会二次预取(double-fetch)已有的数据。
-  // 使用 `router.beforeResolve()`，以便确保所有异步组件都 resolve。
+  // Add router hook for handling asyncData.
+  // Doing it after initial route is resolved so that we don't double-fetch
+  // the data that we already have. Using router.beforeResolve() so that all
+  // async components are resolved.
   router.beforeResolve((to, from, next) => {
     const matched = router.getMatchedComponents(to)
     const prevMatched = router.getMatchedComponents(from)
-    // 我们只关心之前没有渲染的组件
-    // 所以我们对比它们，找出两个匹配列表的差异组件
+
     let diffed = false
     const activated = matched.filter((c, i) => {
       return diffed || (diffed = (prevMatched[i] !== c))
@@ -23,15 +22,20 @@ router.onReady(() => {
     if (!activated.length) {
       return next()
     }
-    // 这里如果有加载指示器(loading indicator)，就触发
+    // start loading indicator
     Promise.all(activated.map(c => {
       if (c.asyncData) {
         return c.asyncData({ store, route: to })
       }
     })).then(() => {
-      // 停止加载指示器(loading indicator)
+      // stop loading indicator
       next()
     }).catch(next)
   })
   app.$mount('#app')
 })
+
+// service worker
+if (window.location.protocol === 'https:' && navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js')
+}
--- src/server-entry.js
+++ src/server-entry.js
@@ -9,7 +9,10 @@ export default context => {
       if (!matchedComponents.length) {
         return reject({ code: 404 })
       }
-      // 对所有匹配的路由组件调用 `asyncData()`
+      // Call fetchData hooks on components matched by the route.
+      // A preFetch hook dispatches a store action and returns a Promise,
+      // which is resolved when the action is complete and store state has been
+      // updated.
       Promise.all(matchedComponents.map(Component => {
         if (Component.asyncData) {
           return Component.asyncData({
@@ -18,11 +21,12 @@ export default context => {
           })
         }
       })).then(() => {
-        // 在所有预取钩子(preFetch hook) resolve 后，
-        // 我们的 store 现在已经填充入渲染应用程序所需的状态。
-        // 当我们将状态附加到上下文，
-        // 并且 `template` 选项用于 renderer 时，
-        // 状态将自动序列化为 `window.__INITIAL_STATE__`，并注入 HTML。
+        // After all preFetch hooks are resolved, our store is now
+        // filled with the state needed to render the app.
+        // Expose the state on the render context, and let the request handler
+        // inline the state in the HTML response. This allows the client-side
+        // store to pick-up the server-side state without having to duplicate
+        // the initial data fetching on the client.
         context.state = store.state
         resolve(app)
       }).catch(reject)

9925249a0d9d46e85378b741fdd4963fe68aba78
doabit <doinsist@gmail.com>
2021-03-08 16:03:25 +0800

    add multiple requests

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -23,9 +23,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  body {
-    background: gray;
-  }
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="block todos">
+      <h1>Todos List</h1>
+      <div v-for="todo in items.todos" :key="todo.id"> <todo :todo="todo" /> </div>
+    </div>
+    <div class="block topics">
+      <h1>Users List</h1>
+      <div v-for="user in items.users" :key="user.id">
+         <h3>{{ user.name }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Todo from '../components/Todo'
+
+export default {
+  components: {
+    todo: Todo
+  },
+
+  computed: {
+    ...mapGetters({
+      items: 'getIndexData'
+    })
+  },
+
+  asyncData ({ store }) {
+    return store.dispatch(`getIndexData`)
+  }
+}
+</script>
+
+<style>
+  .container {
+    display: flex
+  }
+  .block {
+    width: 50%;
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin: 10px;
+  }
+</style>
